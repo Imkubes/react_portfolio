a89c11e29d56829c6e2c097f9f37062ebdc260dd
Imkubes <99090378+Imkubes@users.noreply.github.com>
2022-05-23 16:16:18 -0500

    properly linked linkedin

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,7 +5,7 @@ import {BsGithub} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href='https://www.linkedin.com/in/mike-kubesheski-466b15230/' target="_blank"><BsLinkedin/></a>
+        <a href='https://www.linkedin.com/in/mike-kubesheski/' target="_blank"><BsLinkedin/></a>
         <a href='https://github.com/Imkubes' target="_blank"><BsGithub/></a>
     </div>
   )
